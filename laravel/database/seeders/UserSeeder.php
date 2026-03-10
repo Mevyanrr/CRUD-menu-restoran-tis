c1ee20506b42d627a3a6504c4f69dc8f9dce0d1e
Mevyanrr <najmevya@gmail.com>
2026-03-10 14:05:45 +0700

    fixing

--- laravel/database/seeders/UserSeeder.php
+++ laravel/database/seeders/UserSeeder.php
@@ -2,16 +2,39 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            [
+                'name'       => 'Flora',
+                'username'   => 'flora',
+                'password'   => Hash::make('flora123'),
+                'role'       => 'koki',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                 'name'       => 'Oza',
+                'username'   => 'oza',
+                'password'   => Hash::make('oza123'),
+                'role'       => 'kasir',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                 'name'       => 'Mevya',
+                'username'   => 'mevya',
+                'password'   => Hash::make('mevya123'),
+                'role'       => 'admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
